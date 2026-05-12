dd703abf82064fc05ad23fa6645162126f8cef26
50233806-create <50233806@est.ort.edu.ar>
2026-05-12 09:05:20 -0300

    Add TipoDeComida and Presupuesto properties

--- Models/SugeridorReceta.cs
+++ Models/SugeridorReceta.cs
@@ -4,5 +4,6 @@ public class SugeridorReceta
 {
     public string Nombre { get; set; }
     public DateTime FechaNacimiento { get; set; }
-
+    public string TipoDeComida { get; set; }
+    public string Presupuesto { get; set; }
 }
