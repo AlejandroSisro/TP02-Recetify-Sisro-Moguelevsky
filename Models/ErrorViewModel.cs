cf22d892fe9147afdf256896e2b00774aef901fe
EZEQUIEL ILAN MOGUELEVSKY <50233806@ort>
2026-05-12 09:00:45 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Tp02_Moguelevsky_Sisro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/SugeridorReceta.cs
@@ -0,0 +1,8 @@
+namespace Tp02_Moguelevsky_Sisro.Models;
+
+public class SugeridorReceta
+{
+    public string Nombre { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+
+}
